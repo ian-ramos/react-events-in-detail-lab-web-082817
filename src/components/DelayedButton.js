47dbb886b3985b1a849931390826254d12b9eac9
ian-ramos <ramos.ianc@gmail.com>
2017-10-31 13:58:46 -0400

    Done.

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,17 @@
 // Code DelayedButton Component Here
+import React from 'react';
+
+export default class DelayedButton extends React.Component {
+
+  handleClick = (e) => {
+    e.persist()
+    //setTimeout takes an argument of a function (meaning you don't invoke it), but onDelayedClick needs an argument.  So we pass in an function that points to onDelayedClick so that we can pass onDelayedClick an argument
+    return setTimeout(() => this.props.onDelayedClick(e), this.props.delay)
+  }
+
+  render() {
+    return(
+      <button onClick={this.handleClick}></button>
+    )
+  }
+}
